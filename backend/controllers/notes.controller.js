efbbbac9e20a1a17758be4aa53c8f5ead0148376
Kaushik-codes <kaushiknagofficial@gmail.com>
2026-05-23 22:48:44 +0530

    Read note added

--- backend/controllers/notes.controller.js
+++ backend/controllers/notes.controller.js
@@ -9,9 +9,30 @@ export async function createNote(req, res) {
       content,
     });
 
-    res.status(200).json({message:"Note saved successfully"});
+    res.status(200).json({ message: "Note saved successfully", note });
   } catch (err) {
-    res.status(500).json({message:"Internal server error"});
+    res.status(500).json({ message: "Internal server error" });
+    console.log(err);
+  }
+}
+
+export async function readNote(req, res) {
+  try {
+    const { title } = req.body;
+
+    const note = await notesModel.findOne({ title });
+    const date = new Date(note.updatedAt).toLocaleString();
+
+    res.status(200).json({
+      message: "Note fetched successfully",
+      note: {
+        title: note.title,
+        description: note.content,
+        time: date
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
     console.log(err);
   }
 }
--- backend/routes/notes.route.js
+++ backend/routes/notes.route.js
@@ -4,6 +4,7 @@ import * as notesController from '../controllers/notes.controller.js'
 const router = Router();
 
 router.post('/create-note',notesController.createNote);
+router.get('/read-note',notesController.readNote);
 
 
 export default router;
